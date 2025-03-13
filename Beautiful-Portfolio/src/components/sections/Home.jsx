0f14f6c6ad33a6b7ee2217d683437c48318f6caf
Taylan <k.taylan.acarkan@gmail.com>
2025-03-13 22:00:29 +0300

    Contact kısmı eklendi

--- Beautiful-Portfolio/src/components/sections/Home.jsx
+++ Beautiful-Portfolio/src/components/sections/Home.jsx
@@ -1,9 +1,11 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
 export const Home=()=> {
     return( 
     <section 
     id="home" 
     className="min-h-screen flex items-center justify-center relative"
-    >
+    ><RevealOnScroll>
          <div className="text-center z-10 px-4">
             <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to -cyan-400 bg-clip-text leading-right">
                 Hi,i'm Pedro Tech</h1>
@@ -22,7 +24,7 @@ Contact Me
 </a>
 </div>
 </div>
-
+</RevealOnScroll>
     </section>
 
     );
